df72ce25cb3c4cd0791e6ed00729495f7237411f
Pasith Senevirathna <pasith.info@gmail.com>
2025-04-26 20:13:55 +0530

    feat: integrate Navbar and Footer components into layout and remove from Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,9 @@
 "use client";
 
-import { Footer } from "@/components/footer";
-import { Navbar } from "@/components/navbar";
-
 export default function Home() {
   return (
     <div>
-      <Navbar />
       <h1 className="text-3xl font-bold underline">Home</h1>
-      <Footer />
     </div>
   );
 }
