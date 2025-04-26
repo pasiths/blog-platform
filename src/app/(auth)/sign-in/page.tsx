d0397ff89638f3d5e3d993dd52560c221a42ffcc
Pasith Senevirathna <pasith.info@gmail.com>
2025-04-27 01:33:51 +0530

    feat: add SignIn and SignInForm components for user authentication

---
+++ src/app/(auth)/sign-in/page.tsx
@@ -0,0 +1,21 @@
+import SignInForm from "@/components/auth/sign-in-form";
+
+const SignIn = () => {
+  return (
+    <div className="container mx-auto px-4 w-screen h-[87vh] flex items-center justify-center">
+      <div className="mx-auto flex w-full flex-col justify-center space-y-6">
+        <div className="flex flex-col space-y-2 text-center">
+          <h1 className="text-2xl font-semibold tracking-tight">
+            Sign in to your account
+          </h1>
+          <p className="text-sm text-muted-foreground">
+            Enter your email and password to sign in
+          </p>
+        </div>
+        <SignInForm />
+      </div>
+    </div>
+  );
+};
+
+export default SignIn;
