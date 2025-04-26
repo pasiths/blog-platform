32223f123f209872abd74e51c74d9fa06477b6a7
Pasith Senevirathna <pasith.info@gmail.com>
2025-04-26 19:43:25 +0530

    feat: add Footer component with social media links and update Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { Footer } from "@/components/footer";
 import { Navbar } from "@/components/navbar";
 
 export default function Home() {
@@ -7,6 +8,7 @@ export default function Home() {
     <div>
       <Navbar />
       <h1 className="text-3xl font-bold underline">Home</h1>
+      <Footer />
     </div>
   );
 }
