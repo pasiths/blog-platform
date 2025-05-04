5bc8af47989533e165b829729c586382133864e6
Pasith Senevirathna <pasith.info@gmail.com>
2025-05-04 10:51:48 +0530

    feat(blog): implement GET API for fetching blogs with filtering and pagination

--- src/app/api/blogs/route.ts
+++ src/app/api/blogs/route.ts
@@ -69,3 +69,108 @@ export async function POST(req: NextRequest) {
     );
   }
 }
+
+export async function GET(req: NextRequest) {
+  try {
+    const { searchParams } = new URL(req.url);
+    const title = searchParams.get("title") || undefined;
+    const userId = parseInt(searchParams.get("userId")!) || undefined;
+    const userName = searchParams.get("userName") || undefined;
+    const categoryName = searchParams.get("categoryName") || undefined;
+    const tagName = searchParams.get("tagName") || undefined;
+    const take = searchParams.get("take")
+      ? parseInt(searchParams.get("take")!)
+      : undefined;
+    const postStatus = searchParams.get("postStatus") || PostStatus.APPROVE;
+
+    const blogs = await prisma.post.findMany({
+      where: {
+      AND: [
+        title ? { title: { contains: title, mode: "insensitive" } } : {},
+        userId ? { authorId: userId } : {},
+        userName
+        ? {
+          User: {
+            full_name: { contains: userName, mode: "insensitive" },
+          },
+          }
+        : {},
+        categoryName
+        ? {
+          Category: {
+            some: {
+            name: { contains: categoryName, mode: "insensitive" },
+            },
+          },
+          }
+        : {},
+        tagName
+        ? {
+          Tag: {
+            some: { name: { contains: tagName, mode: "insensitive" } },
+          },
+          }
+        : {},
+        postStatus ? { status: postStatus as PostStatus } : {},
+      ],
+      },
+      orderBy: {
+      createdAt: "desc",
+      },
+      take,
+      include: {
+      User: {
+        select: {
+        id: true,
+        full_name: true,
+        email: true,
+        },
+      },
+      Category: {
+        select: {
+        id: true,
+        name: true,
+        },
+      },
+      Tag: {
+        select: {
+        id: true,
+        name: true,
+        },
+      },
+      Comment: {
+        select: {
+        id: true,
+        content: true,
+        createdAt: true,
+        updatedAt: true,
+        User: {
+          select: {
+          id: true,
+          full_name: true,
+          email: true,
+          },
+        },
+        },
+      },
+      Like: {
+        select: {
+        id: true,
+        userId: true,
+        },
+      },
+      },
+    });
+
+    return NextResponse.json({
+      blogs,
+      message: "Blogs fetched successfully",
+    });
+  } catch (error) {
+    console.log(error);
+    return NextResponse.json(
+      { message: "Error fetching blogs" },
+      { status: 500 }
+    );
+  }
+}
