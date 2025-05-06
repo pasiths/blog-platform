78bd7086b4e4ae9c934b0634f55f44f7836ac168
Pasith Senevirathna <pasith.info@gmail.com>
2025-05-06 20:37:41 +0530

    refactor(api): improve user fetching logic and error handling in GET endpoint

--- src/app/api/users/[id]/route.ts
+++ src/app/api/users/[id]/route.ts
@@ -1,23 +1,55 @@
-import { prisma } from "@/lib/prisma"
-import { type NextRequest, NextResponse } from "next/server"
+import { prisma } from "@/lib/prisma";
+import { type NextRequest, NextResponse } from "next/server";
 
-export async function GET(request: NextRequest, { params }: { params: Promise<{ id: string }> }) {
+export async function GET(
+  request: NextRequest,
+  { params }: { params: Promise<{ id: string }> }
+) {
   try {
-    const { id } = await params 
-
-    const user= await prisma.user.findUnique({
-        where: {
-          id: Number(id),
-        },
-      })
-
-    if (!user) {
-      return NextResponse.json({ error: "User not found" }, { status: 404 })
+    const { id } = await params;
+    if (id) {
+      const userId = Number(id);
+      if (isNaN(userId)) {
+        return NextResponse.json({ error: "Invalid user ID" }, { status: 400 });
+      }
     }
 
-    return NextResponse.json(user)
+    const user = await prisma.user.findUnique({
+      where: {
+        id: Number(id),
+      },
+      include: {
+        Post: {
+          include: {
+            Like: true,
+            Comment: true,
+            Saved: true,
+            User: {
+              select: {
+                id: true,
+                full_name: true,
+                image: true,
+              },
+            },
+            Category:true,
+            Tag: true,
+          },
+        },
+      },
+    });
+
+    if (!user) {
+      return NextResponse.json({ error: "User not found" }, { status: 404 });
+    }
+
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const { password, ...userWithoutPassword } = user;
+    return NextResponse.json(userWithoutPassword);
   } catch (error) {
-    console.error("Error fetching user:", error)
-    return NextResponse.json({ error: "Internal server error" }, { status: 500 })
+    console.error("Error fetching user:", error);
+    return NextResponse.json(
+      { error: "Internal server error" },
+      { status: 500 }
+    );
   }
 }
