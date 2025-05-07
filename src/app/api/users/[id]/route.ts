8cb5b8a034c4b53a0e0dc62be22c7acccd0c24a3
Pasith Senevirathna <pasith.info@gmail.com>
2025-05-07 13:28:15 +0530

    feat(api): enhance PUT endpoint for user updates with validation and error handling

--- src/app/api/users/[id]/route.ts
+++ src/app/api/users/[id]/route.ts
@@ -1,4 +1,5 @@
 import { prisma } from "@/lib/prisma";
+import { getCurrentUser } from "@/server/auth/session";
 import { type NextRequest, NextResponse } from "next/server";
 
 export async function GET(
@@ -31,7 +32,7 @@ export async function GET(
                 image: true,
               },
             },
-            Category:true,
+            Category: true,
             Tag: true,
           },
         },
@@ -47,6 +48,92 @@ export async function GET(
     return NextResponse.json(userWithoutPassword);
   } catch (error) {
     console.error("Error fetching user:", error);
+    return NextResponse.json(
+      { error: "Internal server error" },
+      { status: 500 }
+    );
+  }
+}
+
+export async function PUT(
+  req: NextRequest,
+  { params }: { params: Promise<{ id: string }> }
+) {
+  try {
+    const { id } = await params;
+    if (id) {
+      const userId = Number(id);
+      if (isNaN(userId)) {
+        return NextResponse.json({ error: "Invalid user ID" }, { status: 400 });
+      }
+    }
+
+    const currentUser = await getCurrentUser();
+    if (!currentUser || currentUser.id !== Number(id)) {
+      return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+    }
+
+    const {
+      full_name,
+      bio,
+      location,
+      website,
+      linkedin,
+      instagram,
+      youtube,
+      github,
+      x,
+      image,
+    } = await req.json();
+
+    const user = await prisma.user.findUnique({
+      where: {
+        id: Number(id),
+      },
+    });
+    if (!user) {
+      return NextResponse.json({ error: "User not found" }, { status: 404 });
+    }
+
+    const newUser = await prisma.user.update({
+      where: {
+        id: Number(id),
+      },
+      data: {
+        full_name,
+        bio,
+        location,
+        website,
+        linkedin,
+        instagram,
+        youtube,
+        github,
+        x,
+        image,
+      },
+      select: {
+        id: true,
+        full_name: true,
+        username: true,
+        email: true,
+        bio: true,
+        location: true,
+        website: true,
+        linkedin: true,
+        github: true,
+        instagram: true,
+        youtube: true,
+        x: true,
+        image: true,
+      },
+    });
+    if (!newUser) {
+      return NextResponse.json({ error: "User not found" }, { status: 404 });
+    }
+
+    return NextResponse.json({ newUser });
+  } catch (error) {
+    console.error("Error updating user:", error);
     return NextResponse.json(
       { error: "Internal server error" },
       { status: 500 }
