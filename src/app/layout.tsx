4c8b5762234fe5cb7b9a0576af83c9997892a2ff
Pasith Senevirathna <pasith.info@gmail.com>
2025-05-07 17:10:05 +0530

    feat(ui): add AlertDialog and Toaster components with Radix UI integration

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Navbar } from "@/components/navbar";
 import { Footer } from "@/components/footer";
+import { Toaster } from "@/components/ui/sonner";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -31,6 +32,7 @@ export default function RootLayout({
       >
         <Navbar />
         {children}
+        <Toaster position="top-center" />
         <Footer />
       </body>
     </html>
